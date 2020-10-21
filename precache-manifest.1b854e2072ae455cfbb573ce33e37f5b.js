141cc450b8c05672c74bad87d63f6b16e0a6a15c
Saul Soto <alessaul03@hotmail.com>
2020-10-20 23:00:40 -0500

    Updates

---
+++ precache-manifest.1b854e2072ae455cfbb573ce33e37f5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f193cd229edfbb5424616e586e6a2d1d",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "1c97915854ea13e32001",
+    "url": "/resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9c41897c4bcd1878298a",
+    "url": "/resume/static/js/2.cad115bb.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/resume/static/js/2.cad115bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c97915854ea13e32001",
+    "url": "/resume/static/js/main.4a47b07f.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
