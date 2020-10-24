5ad8890985250077d6cdf9bb776d1b9924319659
Saul Soto <alessaul03@hotmail.com>
2020-10-24 16:28:54 -0500

    added style to skills.js

--- src/screens/content_container/content/skills/skills.js
+++ src/screens/content_container/content/skills/skills.js
@@ -11,15 +11,15 @@ class Skills extends React.Component{
                     {[...new Set(this.props.skills_data.map(r => r.type))].map((type,i) => {return(
                         <div key={i} className='pile'>
                             
-                            <h3 className='pile-title'>{type}</h3>
+                            <p className='pile-title'>{type}</p>
                             
                             {this.props.skills_data.filter(row => {return row.type === type}).map((row,i) => {return(
                                 <div key={i} className='skill'>
-                                    <h3 className='skill-title'>{row.tool}</h3>
+                                    <p className='skill-title'>{row.tool}</p>
                                     {row.modules === null ? null: 
-                                        <div className='susbskills-set'>
+                                        <div className='subskills-set'>
                                             {row.modules.map((module,i) => {return(
-                                                <h3 key={i} className='subskill'>{module}</h3>
+                                                <p key={i} className='subskill'>{module.toLowerCase()}</p>
                                             )})}
                                         </div>
                                     }
