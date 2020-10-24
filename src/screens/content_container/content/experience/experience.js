3e940a25229944e90c459956465ef048059a1b89
Saul Soto <alessaul03@hotmail.com>
2020-10-24 12:05:36 -0500

    created base experience.js

--- src/screens/content_container/content/experience/experience.js
+++ src/screens/content_container/content/experience/experience.js
@@ -8,9 +8,36 @@ class Experience extends React.Component{
                 <h3 className='title'>Experience</h3>
 
                 <div className='category'>
-                    
+
+                    <h3 className='exp-subtitle'>Education</h3>
+                    <div className='exp-cat-detail-container'>
+                        {this.props.experience_data.education.map( (row,i) => {return(
+                            <div key={i} className='exp-cat-detail'>
+                                <p>{row.University}</p>
+                                <p>{row.Degree}</p>
+                                <p>{row.Description}</p>
+                            </div>
+                        )})}
+                    </div>
+
+
                 </div>
                 <div className='category'>
+
+                    <h3 className='exp-subtitle'>Places I've worked in</h3>
+                    <div className='exp-cat-detail-container'>
+                        {this.props.experience_data.jobs.map( (row,i) => {return(
+                            <div key={i} className='exp-cat-detail'>
+                                <p>{row.PositionName}</p>
+                                <p>{row.Company}/{row.DateBegin}-{row.DateEnd}</p>
+                                {row.Responsibilities.map( resp => {return(
+                                    <p>{resp}</p>
+                                )})}
+                                
+                                
+                            </div>
+                        )})}
+                    </div>
 
                 </div>
             </div>
