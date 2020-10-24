99b9a009e92a0a3eeec77c95ba154b07a2efa681
Saul Soto <alessaul03@hotmail.com>
2020-10-24 11:50:39 -0500

    created base experience.js

--- src/screens/content_container/content/experience/experience.js
+++ src/screens/content_container/content/experience/experience.js
@@ -3,9 +3,17 @@ import experience_data from './data.jsx';
 
 class Experience extends React.Component{
     render(){
-        console.log(this.props.experience_data);
         return(
-            <p>Experience</p>
+            <div className='content-experience'>
+                <h3 className='title'>Experience</h3>
+
+                <div className='category'>
+                    
+                </div>
+                <div className='category'>
+
+                </div>
+            </div>
         )
     }
 }
