703fcaa275f2de6632199be5bf142bcb59e54020
Saul Soto <alessaul03@hotmail.com>
2020-10-24 10:38:18 -0500

    added data to introduction.js

--- src/screens/content_container/content/introduction.js
+++ src/screens/content_container/content/introduction.js
@@ -3,9 +3,26 @@ import React from 'react';
 class Introduction extends React.Component{
     render(){
         return(
-            <p>Introduction</p>
+            <div className='content-introduction'>
+                {this.props.intro_data.map( (row,i)  => {return(
+                    <div key={i} className='intro-row'>
+                        <h3 className='intro-title'>{row.title}</h3>
+                        <div className='svg-line'></div>
+                        <p className='intro-description'>{row.desc}</p>
+                    </div>
+                )})}
+            </div>
+            
         )
     }
+}
+
+
+Introduction.defaultProps = {
+    intro_data: [
+        {title:'Profiles', desc:'Data driven and curious about all things mathematics and programming'},
+        {title:'About me', desc:'I\'m a data analyst from Mexico City, 28 years old and currently living in Puerto Vallarta, Mexico'},
+    ]
 }
 
 export default Introduction;
