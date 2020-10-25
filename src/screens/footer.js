c23cf3093273f4526670a682e38255b0e83b6a63
Saul Soto <alessaul03@hotmail.com>
2020-10-24 19:53:06 -0500

    added functionality to "download cv"

--- src/screens/footer.js
+++ src/screens/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import pdf from '../assets/cv.pdf';
 
 class Footer extends React.Component{
     render(){
@@ -6,7 +7,13 @@ class Footer extends React.Component{
             <div className='footer-contact'>
                 <p className='footer-info'>made by me</p>
                 <p className='footer-info'><span>contact</span></p>
-                <p className='footer-info'>download cv</p>
+                <a
+                    className="footer-info"
+                    href={pdf}
+                    download='cv.pdf'
+                >
+                    download cv
+                </a>
             </div>
         )
     }
