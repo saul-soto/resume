087c0527dc65c50d194116ac084df1c7bc49e92b
Saul Soto <alessaul03@hotmail.com>
2020-10-26 14:05:55 -0600

    finished first graph

--- src/screens/content_container/content/gallery/data/Python/montecarlo_simulation/graph.py
+++ src/screens/content_container/content/gallery/data/Python/montecarlo_simulation/graph.py
@@ -4,13 +4,18 @@ from datetime import datetime as dt
 from datetime import timedelta as td
 now = dt.now()
 
-space = create_random_space_walks(.65, 500, 50)
+space = create_random_space_walks(.65, 300, 300)
 
 ls_times = []
 number_simulations = space.shape[1]
 for sim in range(number_simulations):
     now1 = dt.now()
-    plt.plot(space[:,sim], color='blue', alpha=.3)
+    plt.plot(
+        space[:,sim], 
+        color='#740a01', 
+        alpha=.3,
+        linewidth=.5
+    )
 
     ls_times.append( (dt.now()-now1).microseconds )
     print('\tDrawing simulations:\t{}'.format(
@@ -19,6 +24,6 @@ for sim in range(number_simulations):
 
 
 plt.title('Monte Carlo Simulation\n#Simulations: {}'.format(number_simulations))
-plt.savefig('graph.svg', format='svg')
+plt.savefig('graph.svg', format='svg', dpi=1200)
 plt.savefig('graph.png')
 print('\n\tFinished in:\t\t{}'.format(dt.now()-now))
