e59d67b0949eab69af84d924d443727a24bbf5cb
Saul Soto <alessaul03@hotmail.com>
2020-10-26 13:58:00 -0600

    refactor graph.py

--- src/screens/content_container/content/gallery/data/Python/montecarlo_simulation/graph.py
+++ src/screens/content_container/content/gallery/data/Python/montecarlo_simulation/graph.py
@@ -4,22 +4,21 @@ from datetime import datetime as dt
 from datetime import timedelta as td
 now = dt.now()
 
-space = create_random_space_walks(.7, 1000, 10)
+space = create_random_space_walks(.65, 500, 50)
 
 ls_times = []
 number_simulations = space.shape[1]
 for sim in range(number_simulations):
     now1 = dt.now()
-    plt.plot(space[:,sim])
-
-
-
-
+    plt.plot(space[:,sim], color='blue', alpha=.3)
 
     ls_times.append( (dt.now()-now1).microseconds )
     print('\tDrawing simulations:\t{}'.format(
             str(td(microseconds=sum(ls_times)/len(ls_times)) * (number_simulations-sim))
     ), end='\r' if number_simulations != sim else '\n')
 
+
+plt.title('Monte Carlo Simulation\n#Simulations: {}'.format(number_simulations))
+plt.savefig('graph.svg', format='svg')
 plt.savefig('graph.png')
 print('\n\tFinished in:\t\t{}'.format(dt.now()-now))
