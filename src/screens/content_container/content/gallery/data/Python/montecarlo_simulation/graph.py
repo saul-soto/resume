904f05de5caf8b9d6e9e6742f45b81db901cca3f
Saul Soto <alessaul03@hotmail.com>
2020-10-26 17:09:10 -0600

    added function to remove the <metadata> tag to the pyplot svgs

--- src/screens/content_container/content/gallery/data/Python/montecarlo_simulation/graph.py
+++ src/screens/content_container/content/gallery/data/Python/montecarlo_simulation/graph.py
@@ -4,7 +4,7 @@ from datetime import datetime as dt
 from datetime import timedelta as td
 now = dt.now()
 
-space = create_random_space_walks(.65, 300, 300)
+space = create_random_space_walks(probability=.65, steps_to_walk=300, number_simulations=20)
 
 ls_times = []
 number_simulations = space.shape[1]
@@ -20,10 +20,16 @@ for sim in range(number_simulations):
     ls_times.append( (dt.now()-now1).microseconds )
     print('\tDrawing simulations:\t{}'.format(
             str(td(microseconds=sum(ls_times)/len(ls_times)) * (number_simulations-sim))
-    ), end='\r' if number_simulations != sim else '\n')
+    ), end='\n' if number_simulations == sim+1 else '\r')
 
 
 plt.title('Monte Carlo Simulation\n#Simulations: {}'.format(number_simulations))
-plt.savefig('graph.svg', format='svg', dpi=1200)
+plt.savefig('graph.svg', format='svg', dpi=300)
 plt.savefig('graph.png')
+
+# REMOVE THE <metadata> TAG TO AVOID ISSUES WITH IMPORTING
+svg_string = open('graph.svg','r').read()
+svg_string = svg_string.replace('<metadata'+svg_string.split('metadata')[1]+'metadata>','')
+open('graph.svg','+w').write(svg_string)
+
 print('\n\tFinished in:\t\t{}'.format(dt.now()-now))
--- src/screens/content_container/content/gallery/data/data.jsx
+++ src/screens/content_container/content/gallery/data/data.jsx
@@ -2,14 +2,16 @@
 //python:   images/svg's
 //power bi: images(with urls to main report)/embedded report with graphs
 //d3.js:    react components
+import React from 'react';
+import {ReactComponent as MonteCarloSim} from './Python/montecarlo_simulation/graph.svg';
 
 
 const gallery_data = [
     {
         tool:'Python',
-        source:null,
+        source:<MonteCarloSim />,
         type:'svg',
-        description:''
+        description:'This is a montecarlo simulation using Python'
     },
 ]
 
