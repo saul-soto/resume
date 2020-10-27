5fc09f4d9f9326b08b96fb98eaa49dfc37dca8ae
Saul Soto <alessaul03@hotmail.com>
2020-10-27 01:15:46 -0600

    added latex to description

--- src/screens/content_container/content/gallery/data/data.jsx
+++ src/screens/content_container/content/gallery/data/data.jsx
@@ -3,6 +3,8 @@
 //power bi: images(with urls to main report)/embedded report with graphs
 //d3.js:    React Components
 
+import React from 'react';
+import MathJax from 'react-mathjax';
 import {ReactComponent as MonteCarloSim} from './Python/montecarlo_simulation/graph.svg';
 import {ReactComponent as NBABestPlayers} from './Python/top_best_nba_players/graph.svg';
 
@@ -19,7 +21,11 @@ const gallery_data = [
         tool:'Python',
         source:NBABestPlayers,
         type:'svg',
-        title: 'Graph of function\n+'+ String.raw`$$f(x)=\sin²x $$`,
+        title: 
+            <MathJax.Provider>
+                Graph of function 
+                <MathJax.Node formula={`f(x)=\\sin²(x)`} /> 
+            </MathJax.Provider>,
         description:'This is a simple graph of a function using Matplotlib'
     },
 ]
