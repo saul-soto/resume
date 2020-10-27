85ed04bf50d4077b4fb8daab1991b2455b580e5c
Saul Soto <alessaul03@hotmail.com>
2020-10-27 01:47:26 -0600

    added data.js to d3/goodness_of_fit

--- src/screens/content_container/content/gallery/data/D3.js/goodness_of_fit/graph.js
+++ src/screens/content_container/content/gallery/data/D3.js/goodness_of_fit/graph.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import data from './data'
 
 class GoodnessOfFit extends React.Component{
     render(){
+        console.log(data);
         return(
             <svg>
                 <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
