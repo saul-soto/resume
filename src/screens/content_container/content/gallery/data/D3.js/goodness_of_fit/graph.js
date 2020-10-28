f01f76cc7f87b54490308ee45b8f02e129b3b673
Saul Soto <alessaul03@hotmail.com>
2020-10-28 01:38:23 -0600

    added axis to goodness of fit graph

--- src/screens/content_container/content/gallery/data/D3.js/goodness_of_fit/graph.js
+++ src/screens/content_container/content/gallery/data/D3.js/goodness_of_fit/graph.js
@@ -4,19 +4,30 @@ import * as d3 from 'd3';
 
 
 class GoodnessOfFit extends React.Component{
+    constructor(props){
+        super(props);
+        this.data = create_data(30, 1,.5,3)
+    }
+
     render(){
         return(
-            <div className='react-component'></div>
+            <div id='react-component'></div>
         )
     }
 
     _bind_invisibe_elements(){
-        const data = create_data(30, 1,.5,3);
-        console.log(data);
+        d3.select('#react-component').append('svg').attr('id','canvas');
+        const canvas = d3.select('#canvas');
+        canvas.append('line').attr('id','x-axis');
+        canvas.append('line').attr('id','y-axis');
+        canvas.append('line').attr('id','density');
+        canvas.selectAll('circle').data(this.data.vector).enter().append('circle').attr('id','observations');
+        canvas.selectAll('text').data(this.data.bins).enter().append('text').attr('id','x-ticks')
+
     }
 
     _update_sizes(){
-        const node = d3.select('.react-component').node();
+        const node = d3.select('#react-component').node();
         this._update_binded_elements(node.offsetHeight, node.offsetWidth)
     }
 
@@ -27,7 +38,33 @@ class GoodnessOfFit extends React.Component{
     }
 
     _update_binded_elements(height, width){
-        console.log('new sizes',height,width);
+        // PRELIMINARIES
+        d3.select('#canvas')
+            .attr('height', height)
+            .attr('width', width)
+        ;
+
+        const margin_horizontal = 20;
+        const datamin = d3.min(this.data.vector);
+        const datamax = d3.max(this.data.vector);
+
+        const x_scaler = d3.scaleLinear()
+            .domain([datamin, datamax])
+            .range([0+margin_horizontal, width-margin_horizontal])
+
+        // x_axis
+        const axis_height = height -30;
+        d3.select('#x-axis')
+            .attr('x1', x_scaler(datamin)).attr('y1', axis_height)
+            .attr('x2', x_scaler(datamax)).attr('y2', axis_height)
+            .attr('stroke', 'black')
+
+        //y_axis
+        d3.select('#y-axis')
+            .attr('x1', x_scaler(datamin)).attr('y1', 40)
+            .attr('x2', x_scaler(datamin)).attr('y2', axis_height)
+            .attr('stroke', 'black')
+
     }
 }
 
