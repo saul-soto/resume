c4c9964b13434b44bbcaed49f7c385ce48bcaac7
Saul Soto <alessaul03@hotmail.com>
2020-10-28 18:59:17 -0600

    added secure animation to modal && changed footer styles to its own folder

--- src/screens/footer/footer.js
+++ src/screens/footer/footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import pdf from '../../assets/cv.pdf';
 import Modal from './modal';
+import * as d3 from 'd3';
 
 class Footer extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            modal_is_visible: false
+            modal_is_visible: true
         }
     }
 
@@ -17,7 +18,7 @@ class Footer extends React.Component{
                 
                 <p 
                     className='footer-info'
-                    onClick={() => {this.setState({modal_is_visible:true})}}
+                    onClick={this._handleModal.bind(this)}
                 >
                     <span>contact</span>
                 </p>
@@ -30,14 +31,31 @@ class Footer extends React.Component{
                     download cv
                 </a>
 
-                {!this.state.modal_is_visible ? null: 
-                    <Modal 
-                        change_modal_visibility={() => {this.setState({modal_is_visible:!this.state.modal_is_visible})}}
-                    />
-                }
+                <Modal 
+                    change_modal_visibility={this._handleModal.bind(this)}
+                />                
                 
             </div>
         )
+    }
+
+    async _handleModal(){
+        const $modal = d3.select('.contact-modal');
+        const up_or_down = !this.state.modal_is_visible ? 1: -3;
+
+        const do_animation = () => {
+            $modal
+                .transition().duration(500)
+                .style('margin-bottom', (5*up_or_down)+'%')
+            this.setState({modal_is_visible:!this.state.modal_is_visible})
+        }
+
+        await do_animation();
+        $modal.style('display', up_or_down === 1 ? 'flex':'none')
+    }
+
+    componentDidMount(){
+        this._handleModal();
     }
 };
 
