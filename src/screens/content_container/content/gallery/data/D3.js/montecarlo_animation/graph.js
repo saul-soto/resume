d5594cbbd71e4d4454d4ae3fd826d323273ace4c
Saul Soto <alessaul03@hotmail.com>
2020-11-04 22:12:02 -0600

    added slicer to montecarlo_animation.js

--- src/screens/content_container/content/gallery/data/D3.js/montecarlo_animation/graph.js
+++ src/screens/content_container/content/gallery/data/D3.js/montecarlo_animation/graph.js
@@ -12,7 +12,7 @@ class MonteCarloGraph extends React.Component{
             line_coords: [],
             data: [],
             cumsum: [],
-            simulations: 500,
+            simulations: 100,
             probability: .5
         }
     }
@@ -70,11 +70,26 @@ class MonteCarloGraph extends React.Component{
         this._render_graph('update');
     };
 
+    _get_slicer_scaler(){
+        const scaler = 
+            d3.scaleLinear()
+                .domain([1, 2])
+                .range([this.state.width/3, (this.state.width/3)*2 ])
+            ;
+        return scaler;
+    }
+
     _bind_implicit_data(){
         const canvas = d3.select('.canvas');
 
         const input_group_selection = canvas.append('g').attr('class','input-group');
 
+        input_group_selection
+            .selectAll('text').data([null])
+                .enter().append('text')
+                    .attr('class','slicer-title')
+        ;
+        
         input_group_selection
             .selectAll('line').data([null])
                 .enter().append('line')
@@ -85,6 +100,16 @@ class MonteCarloGraph extends React.Component{
             .selectAll('circle').data([this.state.probability])
                 .enter().append('circle')
                     .attr('class','slicer-selector')
+                    .call(d3.drag()
+                        .on("drag", (e) => {
+                            const unit_to_add = e.dx < 0 ? -1:e.dx > 1 ?1:0;
+                            let probability = this.state.probability + unit_to_add/80;
+                            probability = probability > 1? 1: probability < 0 ? 0: probability;
+                            probability = Math.round(probability*100,8)/100;
+                            this.setState({ probability });
+                            this._render_graph('update');
+                        })
+                    )                    
         ;
 
         const axis_group = canvas.append('g').attr('class', 'axis-group');
@@ -107,14 +132,49 @@ class MonteCarloGraph extends React.Component{
 
 
     _render_graph(pattern){
+        const { simulations, width, y_scaler, cumsum, probability} = this.state;
+        const { margin_horizontal, margin_vertical } = this._get_margins();
+        
+        const x_local_scaler = this._get_slicer_scaler();
+
         const merge_enter_update = () =>{
             const line_coords = this.state.line_coords;
+
+            // AXIS
 
             d3.selectAll('.axis')
                 .attr('x1', (_,i) =>{return line_coords[i].x_1}  ).attr('y1', (_,i) =>{return line_coords[i].y_1} )
                 .attr('x2', (_,i) =>{return line_coords[i].x_2}  ).attr('y2', (_,i) =>{return line_coords[i].y_2} )
                 .attr('stroke', 'black')
             ;
+
+            // SLICER GROUP
+            d3.selectAll('.input-group')
+                .attr('transform', `translate(${width/3},${margin_vertical+10})`)
+            ;
+
+
+            d3.selectAll('.slicer-title')
+                .text('Probability: ' + probability*100+'%')
+                .attr('font-size', 14)
+            ;
+                
+            
+            d3.selectAll('.slicer')
+                .attr('x1', x_local_scaler(0)).attr('y1', margin_vertical)
+                .attr('x2', x_local_scaler(1) ).attr('y2', margin_vertical)
+                .attr('stroke','lightgrey')
+                .attr('stroke-width', 3.5)
+            ;
+
+            d3.selectAll('.slicer-selector')
+                .attr('r', 5)
+                .attr('fill', 'gray')
+                .attr('cx', x_local_scaler(this.state.probability))
+                .attr('cy', margin_vertical)                
+            ;
+
+            
         }
 
 
@@ -132,9 +192,6 @@ class MonteCarloGraph extends React.Component{
         else if(pattern==='update'){
             merge_enter_update();
 
-            const { simulations, width, y_scaler, cumsum} = this.state;
-            const { margin_horizontal } = this._get_margins();
-            
             const x_scaler = 
                 d3.scaleLinear()
                     .domain([ 0, simulations - 1])
@@ -184,7 +241,7 @@ class MonteCarloGraph extends React.Component{
         await this._update_sizes();
         window.addEventListener('resize', this._update_sizes.bind(this));
         this._render_graph('enter');
-        this._run_animation(50);
+        this._run_animation(5);
     }
 
 
