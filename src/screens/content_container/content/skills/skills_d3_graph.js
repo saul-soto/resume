c93eb2e0f8eda8a479c943b00c4f3b1f089dcc9c
Saul Soto <alessaul03@hotmail.com>
2020-11-05 19:41:30 -0600

    added responsive functions to skills_d3_graph.js

--- src/screens/content_container/content/skills/skills_d3_graph.js
+++ src/screens/content_container/content/skills/skills_d3_graph.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import content from './data.jsx';
+import * as d3 from 'd3';
 
 class Skills extends React.Component{
+    constructor(props){super(props);
+        this.margins = {horizontal:30, vertical: 30};
+        this.state = {
+            height: null,
+            width: null
+        };
+
+    }
+
     render(){
         const lang = this.props.lang;
 
@@ -10,13 +20,40 @@ class Skills extends React.Component{
                 <h3 className='title'>{content.labels[lang].skill_label}<span>+</span>{content.labels[lang].tool_label}</h3>
 
                 <div id='skills-canvas-container'>
-                    <svg id='skills-canvas'/>
-                    
+                    <svg 
+                        id='skills-canvas'
+                        height = {this.state.height} 
+                        width = {this.state.width}
+                        style = {{border: 'grey', borderStyle:'solid'}}
+                    />                    
                 </div>
 
             </div>
         )
     }
+
+    _bind_implicit_data(){
+
+    }
+
+    _run_pattern(pattern){
+
+    }
+
+    _update_responsive_sizes(){
+        const div_container = d3.select('#skills-canvas-container').node();
+        const height = div_container.offsetHeight-this.margins.vertical;
+        const width = div_container.offsetWidth-this.margins.horizontal;
+        this.setState({ width, height });
+        this._run_pattern('update');
+    }
+
+    componentDidMount(){
+        this._update_responsive_sizes();
+        window.addEventListener('resize', this._update_responsive_sizes.bind(this));
+
+    }
+
 }
 
 export default Skills;
