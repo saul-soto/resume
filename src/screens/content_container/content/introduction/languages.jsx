1208ab7ac0e1f50771fc0ecea71b99df650a62fb
Saul Soto <alessaul03@hotmail.com>
2020-11-11 13:07:02 -0600

    refactor

--- src/screens/content_container/content/introduction/languages.jsx
+++ src/screens/content_container/content/introduction/languages.jsx
@@ -1,11 +1,11 @@
 const content = {
     english:[
         {title:'Profile', desc:'Data driven and curious about all things mathematics and coding'},
-        {title:'About me', desc:'I\'m a data enthusiast from Mexico City, am 28 years old and currently living in Puerto Vallarta, Mexico'},
+        {title:'About me', desc:'I\'m a data enthusiast from Mexico City and currently living in Puerto Vallarta, Mexico'},
     ],
     spanish:[
         {title:'Perfil', desc:'Impulsado por los datos y gran interés sobre las mates y la programación'},
-        {title:'Sobre mi', desc:'Soy un entusiasta de datos de la Ciudad de México, 28 años de edad y viviendo actualmente en Puerto Vallarta, México'},
+        {title:'Sobre mi', desc:'Soy un entusiasta de datos de la Ciudad de México y viviendo actualmente en Puerto Vallarta, México'},
     ],
 }
 
