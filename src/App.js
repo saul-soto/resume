5e4731f4c4354a8aa0bb169712c71cec663beeef
saul-soto <alessaul03@hotmail.com>
2020-11-12 22:16:40 -0600

    added super basic form to sklearn-unsupervised

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles/App.scss';
 import Resume from './screens/resume/main';
 import SklearnUnsupervised from './screens/sklearn-unsupervised/main';
-import SklearnSupervised from './screens/sklearn-unsupervised/main copy'
+// import SklearnSupervised from './screens/sklearn-unsupervised/main copy'
 import { Route } from 'react-router-dom';
 
 class App extends React.Component{
@@ -11,7 +11,7 @@ class App extends React.Component{
 			<>
 				<Route exact path={'/'} component={Resume} />
 				<Route path={'/sklearn-unsupervised'} component={SklearnUnsupervised} />
-				<Route path={'/sklearn-supervised'} component={SklearnSupervised} />
+				{/* <Route path={'/sklearn-supervised'} component={SklearnSupervised} /> */}
 			</>
 		)
 	}
--- src/screens/sklearn-unsupervised/main.js
+++ src/screens/sklearn-unsupervised/main.js
@@ -1,10 +1,52 @@
 import React from 'react';
 
 class SklearnUnsupervised extends React.Component{
+    constructor(props){super(props);
+        this.state = { 
+            score:null,
+            param_1:null,
+            param_2:null,
+        }
+        
+    }
+
+
     render(){
         return(
-            <h1>Sklearn Unsupervised</h1>
+            <div>
+                <div>
+                    <h1>Sklearn Unsupervised</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            param 1:
+                            <input type="text" onChange={(e)=>{this.setState({param_1:e.target.value})}} />
+                        </label>
+                        <label>
+                            param 2:
+                            <input type="text" onChange={(e)=>{this.setState({param_2:e.target.value})}} />
+                        </label>
+
+                        <input 
+                            type="submit" 
+                            value="Submit" 
+                            onClick={()=>{this._get_score(this.state.param_1, this.state.param_2)}}
+                        />
+                    </form>
+                </div>
+
+
+                <div>
+                    <p>Score: {this.state.score}</p>
+                </div>
+
+                
+            </div>
         )
+    }
+
+    _get_score(param_1, param_2){
+        console.log('this are the params', param_1, param_2);
+
     }
 }
 
