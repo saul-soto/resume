928fca03fe389d118f1387d96c8b7500295f2b73
saul-soto <alessaul03@hotmail.com>
2020-11-16 02:20:45 -0600

    skills graph shows in introduction

--- src/screens/resume/content_container/content/introduction/introduction.js
+++ src/screens/resume/content_container/content/introduction/introduction.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import content from './languages.jsx';
+import Skills from '../skills/skills_d3_circular_graph';
 
 class Introduction extends React.Component{
     render(){
         return(
             <div className='content-introduction' id='nav-about'>
-                {content[this.props.lang].map( (row,i)  => {return(
-                    <div key={i} className='intro-row'>
-                        <h3 className='intro-title'>{row.title}</h3>
-                        <div className='svg-line'></div>
-                        <p className='intro-description'>{row.desc}</p>
-                    </div>
-                )})}
+                
+                <div className='introduction-texts-container'>
+                    {content[this.props.lang].map( (row,i)  => {return(
+                        <div key={i} className='intro-row'>
+                            <h3 className='intro-title'>{row.title}</h3>
+                            <div className='svg-line'></div>
+                            <p className='intro-description'>{row.desc}</p>
+                        </div>
+                    )})}
+                </div>
+
+                {/* <Skills lang={this.props.lang}/> */}
+
             </div>
             
         )
