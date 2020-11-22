68053355d0a95ae80d8a613920730163e0da6a9d
saul-soto <alessaul03@hotmail.com>
2020-11-22 17:25:33 -0600

    refactor

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './styles/App.scss';
 import Resume from './screens/resume/main';
 import SklearnUnsupervised from './screens/sklearn-unsupervised/main';
 // import SklearnSupervised from './screens/sklearn-unsupervised/main copy'
-import { Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 class App extends React.Component{
 	render(){
 		return(
-			<>
+			<HashRouter>
 				<Route exact path={'/'} component={Resume} />
 				<Route path={'/sklearn-unsupervised'} component={SklearnUnsupervised} />
 				{/* <Route path={'/sklearn-supervised'} component={SklearnSupervised} /> */}
-			</>
+			</HashRouter>
 		)
 	}
 }
